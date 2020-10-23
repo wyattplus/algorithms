44f2b3ae487679affc3a188b77a5aeeeebe5f875
Wyatt <1140174371@qq.com>
2020-10-23 12:28:00 +0100

    push

---
+++ src/leetcode/study/LeetCode234.java
@@ -0,0 +1,38 @@
+package leetcode.study;
+
+import java.util.Stack;
+
+public class LeetCode234 {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+        ListNode temp = head;
+        Stack<Integer> stack = new Stack<Integer>();
+        int count = 0;
+        while (temp != null) {
+
+            stack.push(temp.val);
+            temp = temp.next;
+            count++;
+        }
+        count = count >> 1;
+        while (count-- >= 0) {
+            if (head.val != stack.pop()) {
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
+    }
+
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
