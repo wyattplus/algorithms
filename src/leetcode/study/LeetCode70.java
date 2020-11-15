6d102ae48a846490826a5c3440c905f2f8a84b21
Wyatt <1140174371@qq.com>
2020-11-15 22:23:56 +0000

    每日一题

--- src/leetcode/study/LeetCode70.java
+++ src/leetcode/study/LeetCode70.java
@@ -22,4 +22,19 @@ public class LeetCode70 {
         }
         return result;
     }
+
+    //practice 2
+    public int climbStairs3(int n) {
+        if (n <= 2) return n;
+        int f1 = 1, f2 = 2, f3 = 0;
+        for (int i = 2; i < n; i++) {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+
+        }
+        return f3;
+    }
+
+
 }
