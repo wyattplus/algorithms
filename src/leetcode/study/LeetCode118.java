32b37a8156299be7a5ef7f357c4bfa135999dbad
Wyatt <1140174371@qq.com>
2020-12-06 17:52:57 +0000

    每日一题

---
+++ src/leetcode/study/LeetCode118.java
@@ -0,0 +1,22 @@
+package leetcode.study;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode118 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numRows; ++i) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                }
+            }
+            ret.add(row);
+        }
+        return ret;
+    }
+}
