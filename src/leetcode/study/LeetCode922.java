9278b63ca29e964bd52b428172d335d79cc49d8c
Wyatt <1140174371@qq.com>
2021-07-03 14:48:35 +0800

    deliberate practice

--- src/leetcode/study/LeetCode922.java
+++ src/leetcode/study/LeetCode922.java
@@ -19,4 +19,32 @@ public class LeetCode922 {
     }
 
 
+    /**
+     * 思路：双指针
+     * 奇数为指针与偶数位指针组合
+     * 步场均=2
+     *
+     * @param A
+     * @return
+     */
+    public int[] sortArrayByParityII2(int[] A) {
+        int fast_index = 1;
+        for (int i = 0; i < A.length; i += 2) {
+            //当偶数位出现奇数时：移动奇数位指针，一直到奇数=偶数值时
+            //交换2位置的值
+            if (A[i] % 2 == 1) {
+                while (A[fast_index] % 2 == 1) {
+                    fast_index += 2;
+                }
+                //交换数值
+                int temp = A[i];
+                A[i] = A[fast_index];
+                A[fast_index] = temp;
+            }
+
+        }
+        return A;
+    }
+
+
 }
