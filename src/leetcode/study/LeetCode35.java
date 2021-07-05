88a2b5e0114bb08e7e5c2e6ddf8c16714ee57c31
Wyatt <1140174371@qq.com>
2021-07-05 15:17:44 +0800

    deliberate practice

---
+++ src/leetcode/study/LeetCode35.java
@@ -0,0 +1,24 @@
+package leetcode.study;
+
+public class LeetCode35 {
+
+    /**
+     * 二分法
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
