41dc98c42347a453853f6355d6a9c507d23354ed
Wyatt <1140174371@qq.com>
2021-07-05 14:41:52 +0800

    deliberate practice

---
+++ src/leetcode/study/LeetCode704.java
@@ -0,0 +1,19 @@
+package leetcode.study;
+
+public class LeetCode704 {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
