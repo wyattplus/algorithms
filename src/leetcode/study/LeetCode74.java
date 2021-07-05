242e2e64dea21b4366e9f1eb296c7acd65867384
Wyatt <1140174371@qq.com>
2021-07-05 11:51:47 +0800

    deliberate practice

--- src/leetcode/study/LeetCode74.java
+++ src/leetcode/study/LeetCode74.java
@@ -21,4 +21,36 @@ public class LeetCode74 {
         }
         return false;
     }
+
+    /**
+     * 思路：二维数组连接成一个一维数组，对该数组进行二分查找
+     *
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        int row = matrix.length;
+        //参数检查
+        if (row == 0) return false;
+        int col = matrix[0].length;
+        //设置二分法 左右指针
+        int left = 0, right = row * col - 1;
+        // 迭代遍历，O（n）复杂度即可
+        int mid = 0, mid_v = 0;
+        while (left <= right) {
+            mid = (left + right) >> 1;
+            mid_v = matrix[mid / col][mid % col];
+            if (mid_v == target) return true;
+            if (mid_v > target) {
+                right = mid - 1;
+
+            } else {
+                left = mid + 1;
+            }
+
+        }
+        return false;
+    }
+
 }
