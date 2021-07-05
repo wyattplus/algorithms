55dbc184523eaee647d9a6822a0566b2d2e864ab
Wyatt <1140174371@qq.com>
2021-07-05 15:06:20 +0800

    deliberate practice

---
+++ src/leetcode/study/LeetCode278.java
@@ -0,0 +1,24 @@
+package leetcode.study;
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+public class LeetCode278 {
+    /**
+     * 二分查找
+     *
+     * @param n
+     * @return
+     */
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
