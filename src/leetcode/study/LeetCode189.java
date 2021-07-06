2224c8cd8f6abb415f2c94db14383826b6c38de2
Wyatt <1140174371@qq.com>
2021-07-06 14:29:54 +0800

    deliberate practice

--- src/leetcode/study/LeetCode189.java
+++ src/leetcode/study/LeetCode189.java
@@ -1,5 +1,7 @@
 package leetcode.study;
 
+import java.util.Arrays;
+
 public class LeetCode189 {
     //prcatice-1 反转思想
     public void rotate(int[] nums, int k) {
@@ -22,5 +24,54 @@ public class LeetCode189 {
         }
     }
 
+    /**
+     * 思路：额外空间存储
+     * 1。初始化额外数组
+     * 2。取余进行下标对应
+     * 3。放回原数组
+     *
+     * @param nums
+     * @param k
+     */
+    public void rotate2(int[] nums, int k) {
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            result[(i + k) % nums.length] = nums[i];
+        }
+        //放回原数组
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = result[i];
 
+        }
+    }
+
+    /**
+     * 思路：翻转数组，然后取段落
+     * 1。整体翻转
+     * 2。0-（k-1）翻转
+     * 3。（k-1）-（n-1）翻转
+     *
+     * @param nums
+     * @param k
+     */
+    public void rotate3(int[] nums, int k) {
+        k = k % nums.length;
+        //翻转可以抽象成函数
+        //整体翻转
+        reverse(nums, 0, nums.length - 1);
+        //头部翻转
+        reverse(nums, 0, k - 1);
+        //尾部翻转
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
 }
